eed457ceb69eac6e0e1a9aeea1cef69f55382cfc
SaputraBR <ahmadbungsu4590@gmail.com>
2022-12-01 15:24:59 +0700

    navbar

---
+++ public/src/custom.js
@@ -0,0 +1,33 @@
+const prevButton = document.getElementById('prevButton');
+const nextButton = document.getElementById('nextButton');
+const slided   = document.querySelectorAll('.slide');
+let curSlide   = 0; 
+let maxSlide   = slided.length - 1;
+
+slided.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${indx * 100}%)`;
+});
+
+nextButton.addEventListener('click', function(){
+    if(curSlide === maxSlide){
+        curSlide = 0;
+    } else {
+        curSlide++;
+    };
+
+    slided.forEach((slide, indx) => {
+        slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+    })
+})
+
+prevButton.addEventListener('click', function(){
+    if(curSlide === 0){
+        curSlide = maxSlide;
+    } else {
+        curSlide--;
+    };
+
+    slided.forEach((slide, indx) => {
+        slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+    })
+})
